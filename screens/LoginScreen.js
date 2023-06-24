17374c5891c3f3a98df47716eed66462cf601dbf
David Wells <david@dw-webdev.com>
2023-06-24 11:12:41 -0400

    week 4 completed

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as ImagePicker from 'expo-image-picker';
 import { baseUrl } from '../shared/baseUrl';
 import logo from '../assets/images/logo.png';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 const LoginTab = ({ navigation }) => {
     const [username, setUsername] = useState('');
@@ -133,7 +134,31 @@ const RegisterTab = () => {
             });
             if(capturedImage.assets) {
                 console.log(capturedImage.assets[0]);
-                setImageUrl(capturedImage.assets[0].uri);
+                processImage(capturedImage.assets[0].uri);
+            }
+        }
+    };
+
+    const processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [{ resize: { width: 400 } }], 
+            { format: ImageManipulator.SaveFormat.PNG }
+        );
+        console.log(processedImage);
+        setImageUrl(processedImage.uri);
+    };
+
+    const getImageFromGallery = async () => {
+        const mediaLibraryPermissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if(mediaLibraryPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if(capturedImage.assets) {
+                console.log(capturedImage.assets[0]);
+                processImage(capturedImage.assets[0].uri);
             }
         }
     };
@@ -147,8 +172,9 @@ const RegisterTab = () => {
                         loadingIndicatorSource={logo}
                         style={styles.image}
                     />
+                    <Button title='Camera' onPress={getImageFromCamera} />
+                    <Button title='Gallery' onPress={getImageFromGallery} />
                 </View>
-                <Button title='camera' onPress={getImageFromCamera} />
                 <Input
                     placeholder='Username'
                     leftIcon={{ type: 'font-awesome', name: 'user-o'}}
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -242,8 +242,21 @@ const Main = () => {
         dispatch(fetchComments());
     }, [dispatch]);
 
+    //useEffect(() => {
+    //    NetInfo.fetch().then(connectionInfo => Platform.OS = 'ios' ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type) : ToastAndroid.show('Initial Network Connectivity Type:' + connectionInfo.type, ToastAndroid.LONG));
+    //    const unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+    //        handleConnectivityChange(connectionInfo);
+    //    });
+    //    return unsubscribeNetInfo;
+    //}, []);
+
+    const showNetInfo = async () => {
+        const connectionInfo = await NetInfo.fetch();
+        Platform.OS = 'ios' ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type) : ToastAndroid.show('Initial Network Connectivity Type:' + connectionInfo.type, ToastAndroid.LONG);
+    };
+
     useEffect(() => {
-        NetInfo.fetch().then(connectionInfo => Platform.OS = 'ios' ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type) : ToastAndroid.show('Initial Network Connectivity Type:' + connectionInfo.type, ToastAndroid.LONG));
+        showNetInfo();
         const unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             handleConnectivityChange(connectionInfo);
         });
